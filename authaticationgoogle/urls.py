8aadc2dce35c3ced622a61ff48d6a42a53ec8b4b
Ramesh244 <ramesh.talluri4@gmail.com>
2024-01-23 12:29:38 +0530

    allauthscoial

---
+++ authaticationgoogle/urls.py
@@ -0,0 +1,30 @@
+# from dj_rest_auth.jwt_auth import get_refresh_view
+# from dj_rest_auth.registration.views import RegisterView
+# from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
+# from django.urls import path
+# from rest_framework_simplejwt.views import TokenVerifyView
+# from authaticationgoogle.views import GoogleLogin
+# from allauth.socialaccount.views import signup
+
+# urlpatterns = [
+#     path("register/", RegisterView.as_view(), name="rest_register"),
+#     path("login/", LoginView.as_view(), name="rest_login"),
+#     path("logout/", LogoutView.as_view(), name="rest_logout"),
+#     path("user/", UserDetailsView.as_view(), name="rest_user_details"),
+#     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+#     path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
+#     path("google/", GoogleLogin.as_view(), name="google_login")
+# ]
+    
+
+
+from django.urls import path
+from .views import *
+
+
+# app_name = 'authaticationgoogle'
+
+urlpatterns = [
+    
+
+]
---
+++ authaticationgoogle/views.py
@@ -0,0 +1,47 @@
+# from django.shortcuts import render
+
+# # Create your views here.
+# from dj_rest_auth.registration.views import SocialLoginView
+# from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+# from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+
+
+# class GoogleLogin(SocialLoginView):
+#     adapter_class = GoogleOAuth2Adapter
+#     # callback_url="http://127.0.0.1:8000/accounts/google/login/callback/"
+#     callback_url = "http://localhost:3000/api/auth/callback/google"
+#     client_class = OAuth2Client
+from django.shortcuts import render
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+import urllib.parse
+
+class GoogleLogin(SocialLoginView):
+# https://dj-rest-auth.readthedocs.io/en/latest/installation.html#social-authentication-optional
+    class GoogleAdapter(GoogleOAuth2Adapter):
+        access_token_url = "https://oauth2.googleapis.com/token"
+        authorize_url = "https://accounts.google.com/o/oauth2/v2/auth"
+        profile_url = "https://www.googleapis.com/oauth2/v2/userinfo"
+    adapter_class = GoogleAdapter
+    # Callback URL was used by mobile app
+    callback_url = "http://localhost:8000/authaticationgoogle/v1/code"
+    client_class = OAuth2Client
+
+
+# Make testing easier
+@api_view(['GET'])
+def CodeView(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        code = urllib.parse.unquote(request.query_params['code'])
+        return Response({
+        	"code":code
+        	})
+
